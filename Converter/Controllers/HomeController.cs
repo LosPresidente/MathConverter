dd8cc79376d9c4cc7ab89199a3e3a973ad110f87
LosPresidente <35542770+LosPresidente@users.noreply.github.com>
2018-03-09 17:08:16 -0600

    upload

---
+++ Converter/Controllers/HomeController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Converter.Models;
+
+namespace Converter.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public IActionResult Convert(Conv conv)
+        {
+
+            switch (conv.ToConvert)
+            {
+                case "mph to km/h":
+                    conv.Result = conv.Cnum * 1.6;
+
+                    break;
+
+                case "km/h to mph":
+                conv.Result = conv.Cnum / 1.6;
+                    break;
+
+                case "C to F":
+                conv.Result = conv.Cnum *1.8 +32;
+                    break;
+
+                case "F to C":
+                conv.Result = (conv.Cnum -32) /1.8;
+                    break;
+            }
+            return View(conv);
+        }
+
+    }
+}
---
+++ Converter/Models/Conv.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace Converter.Models{
+
+    public class Conv
+    {
+        //this one holds what type of operation this will be
+        public string ToConvert{ get; set; }
+        //this one holds the result
+        public double Result { get; set; }
+        //this one holds the input number
+        public float Cnum { get; set; }
+        //this one does not do anything                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                   Idrk stand for I don't really know :)
+        public string Idrk { get; set; }
+        
+    }
+}
